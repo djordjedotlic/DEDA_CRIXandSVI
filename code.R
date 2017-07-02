96c4b5b5e79a2332d2ad409e650ba468136742bb
djordjedotlic <djordje.dotlic@yahoo.com>
2017-07-02 21:16:52 +0200

    Fixes mistakes in the loops

--- code.R
+++ code.R
@@ -1,8 +1,12 @@
+rm(list = ls(all = TRUE))
+graphics.off()
+
 if(!require("strucchange")) install.packages("strucchange"); library("strucchange")
 if(!require("forecast")) install.packages("forecast");library("forecast")
 if(!require("lmtest")) install.packages("lmtest");library("lmtest")
 if(!require("ggplot2")) install.packages("ggplot2");library("ggplot2")
 if(!require("tseries")) install.packages("tseries");library("tseries")
+if(!require("stargazer")) install.packages("stargazer");library("stargazer")
 
 
 #Loading the data----
@@ -24,16 +28,32 @@ bp_ts_svi <- breakpoints(ts_svi~1)
 summary(bp_ts_crix)
 summary(bp_ts_svi)
 
+p <- array()
+p[1] <- bp_ts_svi$breakpoints[1]
+p[2] <- bp_ts_svi$breakpoints[2]
+input$date[p] #dates when structural breaks happened in svi time series
+
+c <- array()
+c[1] <- bp_ts_crix$breakpoints[1]
+c[2] <- bp_ts_crix$breakpoints[2]
+input$date[c] #dates when structural breaks happened in svi time series
+
 #plot the breakpoints 
-plot(input$date, input$crix, xaxt = "n")
-axis.Date(side = 1, input$date, format = "%d/%m/%Y")
-lines(bp_ts_crix)
+bp_date_crix <- array()
+bp_date_crix[1] <- input$date[c[1]]
+bp_date_crix[1] <- as.Date(bp_date_crix[1], format = "%Y-%m-%d")
+bp_date_crix[2] <- input$date[c[2]]
+bp_date_crix[2] <- as.Date(bp_date_crix[2], format = "%Y-%m-%d")
+ggplot( data = input, aes( date, crix )) + geom_line() + geom_vline(xintercept = c(bp_date_crix))
 
 
-plot(input$date, input$svi, xaxt = "n", type="n")
-axis.Date(side = 1, input$date, format = "%d/%m/%Y")
-lines(input$date, input$svi)
-lines(bp_ts_svi)
+
+bp_date <- array()
+bp_date[1] <- input$date[p[1]]
+bp_date[1] <- as.Date(bp_date[1], format = "%Y-%m-%d")
+bp_date[2] <- input$date[p[2]]
+bp_date[2] <- as.Date(bp_date[2], format = "%Y-%m-%d")
+ggplot( data = input, aes( date, svi )) + geom_line() + geom_vline(xintercept = c(bp_date))
 
 
 input$crix <- NULL
@@ -55,8 +75,12 @@ input_list[[2]] <- input_2
 input_list[[3]] <- input_3
 
 
-for(i in 1:3){
-  input <- input_list[[i]]
+granger.caus_p.values <- matrix(nrow = 6, ncol = 2)
+gc_results <- vector(mode = "list",length = 3)
+
+
+for(l in 1:3){
+  input <- input_list[[l]]
   
   #Checking for the stationarity of the series
   ts_returns <- ts(input$returns)
@@ -74,28 +98,42 @@ for(i in 1:3){
   #testing for granger causality
   #the output is a table for each of the periods 
   #identified by the structural break test, respectively 
-  granger.caus_p.values <- matrix(nrow = 9, ncol = 2)
-  colnames(granger.caus_p.values) <- c("svi_runs_returns", "returns_run_svi")
+ 
   
-  for(i in 1:9){
-    gt1 <- grangertest(dreturns ~ dsvi, order=i)
+  for(i in 1:6){
+    gt1 <- grangertest(dreturns ~ dsvi, order= i)
     granger.caus_p.values[i,1] <- gt1$`Pr(>F)`[2]
     
     gt2 <- grangertest(dsvi ~ dreturns, order=i)
     granger.caus_p.values[i,2] <- gt2$`Pr(>F)`[2]
     
   }
-  
-  View (granger.caus_p.values)
-  
+
+  gc_results[[l]] <- granger.caus_p.values
   
 }
 
+gc_results 
+#Gives p-values for granger causality tests, 
+#across the number of lags, across 3 periods
+
 
 #Regression analysis----
+
+input <- read.csv("data/df.csv", header = TRUE, sep = ",")
+input$date <- as.Date(input$date, "%m/%d/%Y")
+input$X.1 <- NULL
+input$crix <- NULL
 input$svi <- log(input$svi)
 input$volume <- log(input$volume)
 input$epuix <- log(input$epuix)
+input_1 <- input[1:91,]
+input_2 <- input[92:124,]
+input_3 <- input[125:146,]
+input_list <- list()
+input_list[[1]] <- input_1
+input_list[[2]] <- input_2
+input_list[[3]] <- input_3
 
 pred_input <- read.csv("data/pred_df.csv", header = TRUE, sep = ",")
 pred_input_1 <- pred_input[1:91,]
@@ -124,3 +162,5 @@ for(i in 1:3){
   bp_pred[[i]] <- bptest(pred_reg)
   
 }
+
+
